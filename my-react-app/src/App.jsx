289d266aa9082acc07d08e4527a30078f967410b
unknown <adj.hamzaoui@gmail.com>
2024-04-02 00:49:29 +0200

    Q8 done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,46 +1,42 @@
-
 import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
-function App () {
-  const [pokemonIndex,setPokemonIndex] = useState(0);
-    return (  
-<div class="container">
-<PokemonCard pokemon={pokemonList[pokemonIndex]} 
-/>
-<NavBar
-pokemonIndex={pokemonIndex}
-setPokemonIndex= {setPokemonIndex}
-pokemonList={pokemonList}
-/>
-</div>
- 
-     );
+function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  return (
+    <div class="container">
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <NavBar
+        
+        setPokemonIndex={setPokemonIndex}
+        pokemonList={pokemonList}
+      />
+    </div>
+  );
 }
-export default App; 
+export default App;
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
--- my-react-app/src/components/NavBar.jsx
+++ my-react-app/src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import Proptypes from "prop-types";
-function NavBar ({pokemonIndex,setPokemonIndex,pokemonList}) {
-    
-    const previousIndex = () =>{setPokemonIndex(pokemonIndex-1)};
-    const nextIndex = () =>{setPokemonIndex(pokemonIndex+1)}
-    console.log(pokemonIndex);
-    return (
-        <div>
-  {pokemonIndex >0 && <button onClick={previousIndex}>Précédent</button>}
-  {pokemonIndex< pokemonList.length-1 && <button onClick={nextIndex}>Suivant</button>}
-     </div>
-    )
+function NavBar({setPokemonIndex, pokemonList }) {
+  
+  return (
+    <div>
+      {pokemonList.map((pokemon,index) => (
+        <button key={pokemon.name} onClick={() =>setPokemonIndex(index,)}>{pokemon.name}</button> 
+        ))}        
+        </div>  )
 }
+
+
+  
+
 export default NavBar;
